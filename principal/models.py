6a7f726b7fd5f0f85d91b8dcd63fe998096d87cb
AleGamero <Mastercadena@hotmail.com>
2018-01-12 10:15:32 +0100

    fix: Añadida Categoria a modelo
    Falta por corregir la lista de productos tras esto.

--- principal/models.py
+++ principal/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
+class Categoria(models.Model):
+    tipo = models.CharField(max_length=50)
+    
+    def __unicode__(self):
+        return str(self.tipo)
+
 class Producto(models.Model):
     name = models.CharField(max_length=64)
     price = models.FloatField()
     descripcion = models.CharField(max_length=500)
-    campos = models.CharField(max_length=50)
+    categorias = models.ManyToManyField(Categoria)
     url = models.URLField()
 
     def __unicode__(self):
--- principal/views.py
+++ principal/views.py
@@ -1,1 +1,15 @@
 # Create your views here.
+
+from principal.models import Producto
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, render_to_response
+from forms import *
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.template.context import RequestContext
+
+def listar_productos(request):
+    productos = Producto.objects.all()
+    return render_to_response('lista_productos.html', {'lista': productos})
