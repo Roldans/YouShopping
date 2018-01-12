aeec453653d39efd7577631c18c4475e522932a2
roldans <roldis2@hotmail.com>
2018-01-12 09:48:02 +0100

    fix: Definir max_length al campo descripcion

--- principal/models.py
+++ principal/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Producto(models.Model):
     name = models.CharField(max_length=64)
     price = models.FloatField()
-    descripcion = models.CharField()
+    descripcion = models.CharField(max_length=500)
     campos = models.CharField(max_length=50)
     url = models.URLField()
 
