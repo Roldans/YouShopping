5063545973412a48485ae9a1d3c69d50bdd86416
AleGamero <Mastercadena@hotmail.com>
2018-01-12 11:35:33 +0100

    fix: Eliminación de Categoria
    Se quito el ManyToMany y dicho modelo.

--- principal/models.py
+++ principal/models.py
@@ -1,16 +1,10 @@
 from django.db import models
 
-class Categoria(models.Model):
-    tipo = models.CharField(max_length=50)
-    
-    def __unicode__(self):
-        return str(self.tipo)
-
 class Producto(models.Model):
     name = models.CharField(max_length=64)
     price = models.FloatField()
     descripcion = models.CharField(max_length=500)
-    categorias = models.ManyToManyField(Categoria)
+    categorias = models.CharField(max_length=500)
     url = models.URLField()
 
     def __unicode__(self):
